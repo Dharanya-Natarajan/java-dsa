a861e426110a18e96bcc5ea8396f5e8dbfe7c1a2
Dharanya-N <dharanyanatarajan944@gmail.com>
2026-07-02 10:22:08 +0530

    AddTwoNum

--- src/basics/AddTwoNum.java
+++ src/basics/AddTwoNum.java
@@ -1,4 +1,13 @@
 package basics;
 
 public class AddTwoNum {
+    public static void main(String[] args){
+        int a;
+        int b;
+        a = 3;
+        b = 4;
+        int c = a+b;
+        System.out.println(c);
+
+    }
 }
